e2bfa9abbbb8bb1080116d6bae2ad5d921d7c78a
Pancho Angarev <pancho.angarev@gmail.com>
2024-03-31 19:56:14 +0300

    Add illustration 404

--- src/pages/PageNotFound.tsx
+++ src/pages/PageNotFound.tsx
@@ -1,5 +1,15 @@
-const PageNotFound = () => {
-    return <div>PageNotFound</div>;
+import MaxWidthWrapper from "./MaxWidthPageWrapper";
+import pageNotFound from "/images/404.svg";
+
+const PageNotFound = (): JSX.Element => {
+  return (
+    <MaxWidthWrapper>
+      <div className="mx-auto flex h-[660px] w-[600px] flex-col items-center gap-8">
+        <img src={pageNotFound} alt="Page not found" />
+        <h1 className="text-4xl font-semibold text-gray-900">Page not found</h1>
+      </div>
+    </MaxWidthWrapper>
+  );
 };
 
 export default PageNotFound;
